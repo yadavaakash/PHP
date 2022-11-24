f5f5f7037834f396beda449c88395d2d8d3b12df
Akash Yadav <92569354+yadavaakash@users.noreply.github.com>
2022-11-24 13:45:10 +0530

    Delete Extraworksphp/Extraworksphp directory

--- Extraworksphp/Extraworksphp/areaofcircle.php
+++
@@ -1,9 +0,0 @@
-<!-- 3. Calculate area of a circle. -->
-<?php
-    $pai = 3.14;
-    $r = 5;
-
-    $a = $pai*$r*$r;
-
-    echo "Area of circle  A = &pi; R&sup2; :- " ,$a;
-?>
--- Extraworksphp/Extraworksphp/tence.php
+++
@@ -1,7 +0,0 @@
-<!-- 18. Take a number. Generate multiplication table of that number. -->
-<?php
-  $a = 6;
-  for($i = 1; $i <= 20; $i++){
-    echo $a ," X ",$i," = ",$a * $i,"<br>";
-  }
-?>
